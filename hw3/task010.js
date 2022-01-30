af0eda10effd5b15084345a5aab7109d7cafbf47
ZakharenkoYaroslav <92329099+ZakharenkoYaroslav@users.noreply.github.com>
2022-01-31 00:01:56 +0200

    init

--- hw3/task010.js
+++ hw3/task010.js
@@ -1,10 +1,22 @@
 
 var reverse = function (array) {
-    if (array.length =0) {
+    if (Array.isArray(array)) {
+        if (array.length != 0) {
+            var array2 = [];
 
+            for (i = array.length-1; i > -1; i--){
+                array2.push(array[i]);
+            }
+            console.log(array2);
+        } else {
+            console.log("Масив пустий!")
+        }
     }
     else {
-        console.log("Масив пустий!")
+        console.log("Параметр не є масивом!")
     }
     
 }
+
+const arr = [3,2,1];
+reverse(arr); // [1,2,3]
